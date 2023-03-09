0eb5921edc5555f749e28561a42e4f11e258d546
LonelyPorter <76800139+LonelyPorter@users.noreply.github.com>
2023-03-08 21:53:15 -0500

    feat: add download

---
+++ download.py
@@ -0,0 +1,64 @@
+from typing import List
+import os
+import io
+from google_utils import authenticate
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from googleapiclient.http import MediaIoBaseDownload
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/drive']
+
+
+def download(from_folder: str, to_folder: str = "downloads") -> bool:
+    if not os.path.exists(to_folder):
+       os.makedirs(to_folder)
+
+    try:
+      query = "mimeType='application/vnd.google-apps.folder' and trashed = false and name='{}'".format(
+          from_folder)
+      results = service.files().list(q=query, fields='nextPageToken, files(id, name)').execute()
+      
+      if len(results['files']) == 0:
+         print("Folder not exists!")
+         return False
+      
+      folder_id = results['files'][0]['id']
+
+      # Search for files in the folder
+      query = "'{}' in parents and trashed=false".format(folder_id)
+      results = service.files().list(
+          q=query, fields="nextPageToken, files(id, name)").execute()
+
+      # Download each file in the folder
+      for file in results.get("files", []):
+          file_id = file["id"]
+          file_name = file["name"]
+          file_path = os.path.join(to_folder, file_name)
+
+          request = service.files().get_media(fileId=file_id)
+          buffer = io.BytesIO()
+          downloader = MediaIoBaseDownload(buffer, request)
+
+          done = False
+          while not done:
+             status, done = downloader.next_chunk()
+             print("Download {}% for file '{}'.".format(int(status.progress() * 100), file_name))
+
+          with open(file_path, 'wb') as f:
+             f.write(buffer.getvalue())
+
+          print("File '{}' downloaded to '{}'.".format(file_name, file_path))
+
+
+    except HttpError as error:
+      print(f"An error occured: {error}")
+      return False
+
+    return True
+
+if __name__ == "__main__":
+    creds = authenticate(SCOPES)
+    service = build('drive', 'v3', credentials=creds)
+    download('Secrect Pictures')
+
